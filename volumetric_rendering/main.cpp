36e980ddb9a6368600c2025228fd0f8489fe3e7c
Dmitri Wamback <56200546+DmitriWamback@users.noreply.github.com>
2025-06-10 08:39:12 -0400

    Initial Commit

---
+++ volumetric_rendering/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  volumetric_rendering
+//
+//  Created by Dmitri Wamback on 2025-06-10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
